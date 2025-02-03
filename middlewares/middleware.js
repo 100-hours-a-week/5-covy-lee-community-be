178129500ff5688f6a5569b2b0c760d6a454e636
이민구 <goorm@goormui-MacBookPro.local>
2025-02-03 17:08:03 +0900

    add: image CDN

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -1,4 +1,18 @@
+require('dotenv').config();
+
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const multer = require('multer');
+const multerS3 = require('multer-s3');
+
+// S3 클라이언트 생성 (AWS SDK v3)
+const s3 = new S3Client({
+    region: 'ap-northeast-2',
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+});
+
 const fs = require('fs');
 const path = require('path');
 
@@ -22,21 +36,32 @@ const profileUpload = multer({
     limits: { fileSize: 5 * 1024 * 1024 } // 5MB 제한
 });
 
-// post_images 폴더 설정
-const postUploadDir = path.join(__dirname, '../post_images');
-if (!fs.existsSync(postUploadDir)) {
-    fs.mkdirSync(postUploadDir); // 폴더가 없으면 생성
-}
+// // post_images 폴더 설정
+// const postUploadDir = path.join(__dirname, '../post_images');
+// if (!fs.existsSync(postUploadDir)) {
+//     fs.mkdirSync(postUploadDir); // 폴더가 없으면 생성
+// }
+//
+// // multer 설정 (post_images)
+// const postStorage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, postUploadDir); // post_images 디렉토리에 저장
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, Date.now() + '-' + file.originalname); // 파일 이름 설정
+//     }
+// });
 
-// multer 설정 (post_images)
-const postStorage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, postUploadDir); // post_images 디렉토리에 저장
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname); // 파일 이름 설정
+const postStorage = multerS3({
+    s3: s3,
+    bucket: 'covy-uploads',
+    contentType: multerS3.AUTO_CONTENT_TYPE,  // 자동 content-type 설정
+    key: function (req, file, cb) {
+        cb(null, `post_images/${Date.now()}-${file.originalname}`);
     }
 });
+
+
 const postUpload = multer({
     storage: postStorage,
     limits: { fileSize: 5 * 1024 * 1024 } // 5MB 제한
